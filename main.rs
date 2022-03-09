9fe0d5097c50674f40c88cda1509c164cee05e31
navigaid <navigaid@gmail.com>
2022-03-09 14:27:56 +0800

    use cronos + clap

--- main.rs
+++ main.rs
@@ -1,14 +1,57 @@
-use std::collections::BTreeMap;
+use chrono::Datelike;
+use clap::Parser;
 use handlebars::Handlebars;
+use std::collections::BTreeMap;
 
 const TEMPLATE: &str = include_str!("./template.hbs");
 
-fn main(){
-    let mut handlebars = Handlebars::new();
-    assert!(handlebars.register_template_string("template", TEMPLATE).is_ok());
-    let mut data = BTreeMap::new();
-    data.insert("year".to_string(), "2022".to_string());
-    data.insert("fullname".to_string(), "btwiuse".to_string());
-    let out = handlebars.render("template", &data).unwrap();
-    print!("{}", out);
+#[derive(Debug, Clone, Parser)]
+#[clap(
+    name = "mit",
+    author = "btwiuse <btwiuse@gmail.com>",
+    about = "generate MIT license",
+    version
+)]
+pub struct App {
+    #[clap(short = 'y', long = "year", value_name = "YEAR", help = "Year")]
+    year: Option<String>,
+    #[clap(
+        short = 'a',
+        long = "author",
+        value_name = "AUTHOR",
+        help = "Author name"
+    )]
+    author: String,
+}
+
+fn this_year() -> i32 {
+    let current_date = chrono::Utc::now();
+    current_date.year()
+}
+
+impl App {
+    fn run(&self) {
+        print!("{}", self.to_string());
+    }
+    fn to_string(&self) -> String {
+        let mut handlebars = Handlebars::new();
+        assert!(handlebars
+            .register_template_string("template", TEMPLATE)
+            .is_ok());
+        let mut data = BTreeMap::new();
+
+        let year = self.year.clone();
+        let year = year.unwrap_or(format!("{}", this_year()));
+        data.insert("year".to_string(), year);
+
+        let author = self.author.clone();
+        data.insert("author".to_string(), author);
+
+        handlebars.render("template", &data).unwrap()
+    }
+}
+
+fn main() {
+    let app = App::parse();
+    app.run()
 }
